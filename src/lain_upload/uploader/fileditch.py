08267ada0d64ec481f99ac609056425c3c7a9f54
NecRaul <necraul@kuroneko.dev>
2026-05-20 19:40:44 +0400

    Adapt FileDitchUploader to new API endpoint and response schema

--- src/lain_upload/uploader/fileditch.py
+++ src/lain_upload/uploader/fileditch.py
@@ -4,14 +4,14 @@ from .base import BaseUploader
 class FileDitchUploader(BaseUploader):
     def __init__(self, file_path):
         self.file_path = file_path
-        self.file_max_size = 15 * 1000 * 1000 * 1000
-        self.file_max_size_str = "15GB"
+        self.file_max_size = 100 * 1000 * 1000 * 1000
+        self.file_max_size_str = "100GB"
         self.http_method = "POST"
-        self.api_endpoint = "https://up1.fileditch.com/upload.php"
+        self.api_endpoint = "https://new.fileditch.com/upload.php"
 
     def _build_fields(self, file_name, file):
-        return {"files[]": (file_name, file)}
+        return {"file": (file_name, file)}
 
     @staticmethod
     def _extract_url(response):
-        return response.json()["files"][0]["url"]
+        return response.json()["url"]
